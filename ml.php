522e6af1b69ab1041cb250dc3cdc73d28000f95b
Alexandr Generalov <generalov.a@medbooking.com>
2016-11-01 18:55:24 +0300

    works

---
+++ ml.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @see https://stovepipe.systems/post/machine-learning-naive-bayes
+ */
+
+class Classifier
+{
+    private $types     = [Type::POSITIVE, Type::NEGATIVE];
+    private $words     = [Type::POSITIVE => [], Type::NEGATIVE => []];
+    private $documents = [Type::POSITIVE => 0, Type::NEGATIVE => 0];
+
+    public function guess($statement)
+    {
+        $words           = $this->getWords($statement); // get the words
+        $best_likelihood = 0;
+        $best_type       = null;
+
+        foreach ($this->types as $type) {
+            $likelihood = $this->totalP($type); // calculate P(Type)
+
+            foreach ($words as $word) {
+                $likelihood *= $this->p($word, $type); // calculate P(word, Type)
+            }
+
+            if ($likelihood > $best_likelihood) {
+                $best_likelihood = $likelihood;
+                $best_type       = $type;
+            }
+        }
+
+        return $best_type;
+    }
+
+    public function learn($statement, $type)
+    {
+        $words = $this->getWords($statement);
+
+        foreach ($words as $word) {
+            if (!isset($this->words[$type][$word])) {
+                $this->words[$type][$word] = 0;
+            }
+            $this->words[$type][$word]++; // increment the word count for the type
+        }
+        $this->documents[$type]++; // increment the document count for the type
+    }
+
+    public function totalP($type)
+    {
+        return ($this->documents[$type] + 1) / (array_sum($this->documents) + 1);
+    }
+
+    public function p($word, $type)
+    {
+        $count = isset($this->words[$type][$word]) ? $this->words[$type][$word] : 0;
+
+        return ($count + 1) / (array_sum($this->words[$type]) + 1);
+    }
+
+    public function getWords($string)
+    {
+        return preg_split('/\s+/', preg_replace('/[^А-Яа-яA-Za-z0-9\s]/', '', strtolower($string)));
+    }
+}
+
+class Type
+{
+    const POSITIVE = 'positive';
+    const NEGATIVE = 'negative';
+}
+
+$classifier = new Classifier();
+$classifier->learn('Symfony is the best', Type::POSITIVE);
+$classifier->learn('PhpStorm is great', Type::POSITIVE);
+$classifier->learn('Iltar complains a lot', Type::NEGATIVE);
+$classifier->learn('No Symfony is bad', Type::NEGATIVE);
+
+
+var_dump($classifier->guess('Symfony is great')); // string(8) "positive"
+var_dump($classifier->guess('I complain a lot')); // string(8) "negative"
